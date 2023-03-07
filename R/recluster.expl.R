3f45ae9d2162ea013b2418be2990dc95fd5fffce
Leonardo Dapporto <68218234+leondap@users.noreply.github.com>
2023-03-07 10:00:40 +0100

    Update recluster.expl.R

--- R/recluster.expl.R
+++ R/recluster.expl.R
@@ -1,14 +1,15 @@
 recluster.expl<-function(mat,clust){
 mat<-as.matrix(mat)
-beta <- sum(mat)
-cluster <- NULL
+beta <- sum(mat, na.rm=T)
 betapartial <- 0
         for (row in 1:nrow(mat)) {
             for (col in 1:ncol(mat)) {
-                if (clust[row] != clust[col]) {
+			if (!(is.na(mat[row,col])) & !(is.na(clust[row])) & !(is.na(clust[col]))) {
+               	 	if (clust[row] != clust[col]) {
                   betapartial <- betapartial + mat[row, col]
                 }
             }
         }
+	}
 return(betapartial/beta)
 }
