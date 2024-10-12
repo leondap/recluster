f9c7bd08b2e3a25b79051ca95cd1f39639cd99db
Leonardo Dapporto <68218234+leondap@users.noreply.github.com>
2024-10-12 14:21:27 +0200

    Update biodecrypt.view.R

--- R/biodecrypt.view.R
+++ R/biodecrypt.view.R
@@ -1,4 +1,3 @@
 biodecrypt.view<-function(mat,id,alpha=NULL, fraction=0.95, partCount=10, map=NULL,main=NULL, xlim=NULL,ylim=NULL,clipToCoast="terrestrial",cex0=0.2,cex1=0.25){
 	stop("biodecrypt.view is deprecated and no longer supported. Please use biodecrypt with plot=T to achieve the same result. This function will be removed in future versions.")
-  # Code for the old function (if necessary)
 }
