041ebb505bd99f75ff0cb709cb912f58bb1f0faa
Leonardo Dapporto <68218234+leondap@users.noreply.github.com>
2024-11-09 11:03:46 +0100

    Update recluster.expl.R

--- R/recluster.expl.R
+++ R/recluster.expl.R
@@ -1,7 +1,7 @@
-recluster.expl<-function(dist,clust){
-	mat<-as.vector(dist)
-	same<-as.vector(dist(clust))
-	beta <- sum(mat)
-	partial<-sum(mat[which(same>0)])
-	return(partial/beta)
+recluster.expl<-function (dist, clust) {
+    mat <- as.vector(dist)
+    same <- as.vector(dist(clust))
+    beta <- sum(mat, na.rm=T)
+    partial <- sum(mat[which(same > 0)], na.rm=T)
+    return(partial/beta)
 }
