38287461a758b56f8a53809ec6e9cab92f2b5727
Leonardo Dapporto <68218234+leondap@users.noreply.github.com>
2026-06-20 22:43:07 +0200

    Update biodecrypt.optimise.R

--- R/biodecrypt.optimise.R
+++ R/biodecrypt.optimise.R
@@ -1,33 +1,58 @@
-biodecrypt.optimise<-function(tab,coef=c(2,1,1), penalty=10){
-  	res<-NULL
-	val<-tab[,1]^coef[1]+tab[,2]^coef[2]+tab[,3]^coef[3]
-	mini<-min(val)
-	quali<-which((val-mini)<penalty)
-	if(length(quali)>1){
-		tot<-sum(1/val[quali])
-		weight<-tab[quali,(4:ncol(tab))]/val[quali]
-		res$ratio<-(sum(weight[,1]))/tot
-		res$buffer<-(sum(weight[,2]))/tot
-		res$MIR<-mean(tab[quali,1])
-		res$NIR<-mean(tab[quali,2])
-		res$NUR<-mean(tab[quali,3])
-		quantialpha<-ncol(weight)-2
-		res$alpha<-NULL
-			for(q in 1:quantialpha){
-			res$alpha[q]<-(sum(weight[,(2+q)]))/tot
-		}
-	}
-	if(length(quali)==1){
-		res$ratio<-tab[quali,4]
-		res$buffer<-tab[quali,5]
-		res$MIR<-tab[quali,1]
-		res$NIR<-tab[quali,2]
-		res$NUR<-tab[quali,3]
-		quantialpha<-ncol(tab)-5
-		res$alpha<-NULL
-			for(q in 1:quantialpha){
-			res$alpha[q]<-tab[quali,(5+q)]	
-		}
-	}
-	return(res)
+biodecrypt.optimise <- function(tab, coef = c(2, 1, 1), penalty = 10) {
+  
+  res <- list()
+  
+  if (ncol(tab) < 6) {
+    stop("tab must contain at least MIR, NIR, NUR, ratio, buffer and one alpha column.")
+  }
+  
+  tab <- as.data.frame(tab)
+  
+  MIR <- as.numeric(tab[, 1])
+  NIR <- as.numeric(tab[, 2])
+  NUR <- as.numeric(tab[, 3])
+  
+  score <- MIR^coef[1] + NIR^coef[2] + NUR^coef[3]
+  
+  mini <- min(score, na.rm = TRUE)
+  quali <- which((score - mini) < penalty)
+  
+  if (length(quali) == 0) {
+    stop("No valid optimisation rows found.")
+  }
+  
+  if (length(quali) == 1) {
+    
+    res$ratio <- as.numeric(tab[quali, 4])
+    res$buffer <- as.numeric(tab[quali, 5])
+    res$MIR <- MIR[quali]
+    res$NIR <- NIR[quali]
+    res$NUR <- NUR[quali]
+    res$alpha <- as.numeric(tab[quali, 6:ncol(tab)])
+    
+  } else {
+    
+    score_sel <- score[quali]
+    w <- 1 / score_sel
+    w <- w / sum(w, na.rm = TRUE)
+    
+    res$ratio <- sum(as.numeric(tab[quali, 4]) * w, na.rm = TRUE)
+    res$buffer <- sum(as.numeric(tab[quali, 5]) * w, na.rm = TRUE)
+    
+    res$MIR <- mean(MIR[quali], na.rm = TRUE)
+    res$NIR <- mean(NIR[quali], na.rm = TRUE)
+    res$NUR <- mean(NUR[quali], na.rm = TRUE)
+    
+    alpha_mat <- as.matrix(tab[quali, 6:ncol(tab), drop = FALSE])
+    storage.mode(alpha_mat) <- "numeric"
+    
+    res$alpha <- colSums(alpha_mat * w, na.rm = TRUE)
+  }
+  
+  res$score <- mini
+  res$selected_rows <- quali
+  res$penalty <- penalty
+  res$coef <- coef
+  
+  return(res)
 }
