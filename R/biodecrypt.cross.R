25cb7c57407dff5f792302191d4bd8262a3ce2f2
Leonardo Dapporto <68218234+leondap@users.noreply.github.com>
2026-06-20 22:24:50 +0200

    Update biodecrypt.cross.R

--- R/biodecrypt.cross.R
+++ R/biodecrypt.cross.R
@@ -1,62 +1,151 @@
 
-biodecrypt.cross<-function(mat,id,alpha=NULL,ratio=2.5,buffer=90, checkdist=T, minimum=7, polygon=NULL, map=NULL,xlim=NULL,ylim=NULL,main=NULL,runs=10,test=T){
-	res<-NULL
-	res$type<-"cross"
-	taxa<-max(id)
-	idr<-id[which(id>0)]
-	matrix<-mat[which(id>0),]
-	if(is.null(alpha)){
-		alpha=rep(8,taxa)
-	}
-	q<-aggregate(rep(1,length(idr))~idr,FUN="sum")
-	min<-which(q[,2]==min(q[,2]))
-	if(min(q[,2])<4){
-		stop("The cross validation procedure requires a minimum of 4 distinct points")
-	}
-	if(test){
-	test_run<-biodecrypt(mat,id,alpha=alpha,ratio=ratio,buffer=buffer,map=map,polygon=polygon, checkdist=checkdist, xlim=xlim,ylim=ylim,main=main)
-	res$NUR<-test_run$NUR
-	res$areas<-test_run$areas
-	res$intersections<-test_run$intersections
-	res$sympatry<-test_run$sympatry
-	res$table<-test_run$table
-	}
-	species<-unique(idr)
-	which_sp<-NULL
-	how_may<-NULL
-	order<-NULL
-	for (sp in 1:length(species)){
-		which_sp[[sp]]<-which(idr==species[sp])[sample(1:length(which(idr==species[sp])))]
-		how_may[[sp]]<-length(which_sp[[sp]])/runs
-		order<-c(order,which_sp[[sp]])
-		}
-	matrixnewbs<-cbind(matrix[order,],idr[order])
-	attr<-rep(NA, nrow(matrix))
-	for (giro in 1:runs){
-		first<-NULL
-		last<-NULL
-		memberbs<-matrixnewbs[,3]
-		for (spe in 1:length(species)){
-			#spe<-2
-			first[spe]<-round(1+((giro-1)*as.numeric(how_may[spe])))
-			last[spe]<-round(giro*as.numeric(how_may[spe]),0)
-			qualitot<-which(matrixnewbs[,3]==species[spe])
-			if(first[spe]<=last[spe]){
-			memberbs[qualitot[first[spe]:last[spe]]]<-0
-			if(giro==runs){
-			  memberbs[qualitot[first[spe]:qualitot[length(qualitot)]]]<-0
-			}
-			}
-		}
-		finalebs<-biodecrypt(matrixnewbs[,c(1,2)], memberbs, alpha=alpha, minimum=minimum, ratio=ratio,buffer=buffer, checkdist=checkdist,polygon=polygon, map=map,xlim=xlim,ylim=ylim,main=main) 
-		attri<-which(memberbs==0)
-		attr[attri]<-finalebs$table[attri,3]
-	}
-	res$cross<-cbind(matrixnewbs[,3],attr,rep(0,nrow(matrix)),rep(0,nrow(matrix)), matrixnewbs[,1:2])
-	colnames(res$cross)<-c("original","predicted","MIR","NIR","Long","Lat")
-	res$cross[which(res$cross[,2]>0 & res$cross[,1]!=res$cross[,2]),3]<-1
-	res$cross[which(res$cross[,2]==0),4]<-1
-	res$MIR<-(sum(res$cross[,3])/nrow(res$cross))*100
-	res$NIR<-(sum(res$cross[,4])/nrow(res$cross))*100
-	return(res)
+biodecrypt.cross <- function(
+  mat, id, alpha = NULL, ratio = 2.5, buffer = 90,
+  checkdist = TRUE, minimum = 7, polygon = NULL,
+  map = NULL, xlim = NULL, ylim = NULL, main = NULL,
+  runs = 10, test = TRUE, crs_area = NULL
+) {
+  
+  res <- list()
+  res$type <- "cross"
+  
+  colnames(mat) <- c("Long", "Lat")
+  
+  unknown <- is.na(id) | id == 0 | id == "0"
+  taxa_ids <- sort(unique(id[!unknown]))
+  taxa <- length(taxa_ids)
+  
+  if (taxa < 2) {
+    stop("Cross validation requires at least two known taxa.")
+  }
+  
+  id_internal <- rep(0, length(id))
+  for (j in seq_along(taxa_ids)) {
+    id_internal[id == taxa_ids[j]] <- j
+  }
+  
+  use_known <- which(id_internal > 0)
+  idr <- id_internal[use_known]
+  matrix <- mat[use_known, , drop = FALSE]
+  
+  if (is.null(alpha)) {
+    alpha <- rep(8, taxa)
+  }
+  
+  if (length(alpha) == 1) {
+    alpha <- rep(alpha, taxa)
+  }
+  
+  if (length(alpha) != taxa) {
+    stop("alpha must have length 1 or the same number of known taxa.")
+  }
+  
+  q <- aggregate(rep(1, length(idr)) ~ idr, FUN = "sum")
+  
+  if (min(q[, 2]) < 4) {
+    stop("The cross validation procedure requires a minimum of 4 distinct points per taxon.")
+  }
+  
+  if (test) {
+    test_run <- biodecrypt(
+      mat, id,
+      alpha = alpha,
+      ratio = ratio,
+      buffer = buffer,
+      map = map,
+      polygon = polygon,
+      checkdist = checkdist,
+      minimum = minimum,
+      xlim = xlim,
+      ylim = ylim,
+      main = main,
+      crs_area = crs_area
+    )
+    
+    res$NUR <- test_run$NUR
+    res$areas <- test_run$areas
+    res$intersections <- test_run$intersections
+    res$sympatry <- test_run$sympatry
+    res$table <- test_run$table
+  }
+  
+  species <- sort(unique(idr))
+  
+  which_sp <- vector("list", length(species))
+  how_many <- numeric(length(species))
+  ord <- NULL
+  
+  for (sp in seq_along(species)) {
+    idx <- which(idr == species[sp])
+    which_sp[[sp]] <- sample(idx)
+    how_many[sp] <- length(which_sp[[sp]]) / runs
+    ord <- c(ord, which_sp[[sp]])
+  }
+  
+  matrixnewbs <- cbind(matrix[ord, , drop = FALSE], idr[ord])
+  colnames(matrixnewbs) <- c("Long", "Lat", "id")
+  
+  attr <- rep(NA, nrow(matrixnewbs))
+  
+  for (giro in seq_len(runs)) {
+    
+    memberbs <- matrixnewbs[, "id"]
+    
+    for (spe in seq_along(species)) {
+      
+      qualitot <- which(matrixnewbs[, "id"] == species[spe])
+      
+      first <- floor((giro - 1) * length(qualitot) / runs) + 1
+      last <- floor(giro * length(qualitot) / runs)
+      
+      if (first <= last) {
+        memberbs[qualitot[first:last]] <- 0
+      }
+    }
+    
+    finalebs <- biodecrypt(
+      matrixnewbs[, c("Long", "Lat"), drop = FALSE],
+      memberbs,
+      alpha = alpha,
+      minimum = minimum,
+      ratio = ratio,
+      buffer = buffer,
+      checkdist = checkdist,
+      polygon = polygon,
+      map = map,
+      xlim = xlim,
+      ylim = ylim,
+      main = main,
+      crs_area = crs_area
+    )
+    
+    attri <- which(memberbs == 0)
+    
+    if (!is.null(finalebs$table_internal)) {
+      attr[attri] <- finalebs$table_internal[attri, "id2_internal"]
+    } else {
+      attr[attri] <- finalebs$table[attri, 3]
+    }
+  }
+  
+  res$cross <- cbind(
+    original = matrixnewbs[, "id"],
+    predicted = attr,
+    MIR = rep(0, nrow(matrixnewbs)),
+    NIR = rep(0, nrow(matrixnewbs)),
+    Long = matrixnewbs[, "Long"],
+    Lat = matrixnewbs[, "Lat"]
+  )
+  
+  res$cross[which(res$cross[, "predicted"] > 0 &
+                    res$cross[, "original"] != res$cross[, "predicted"]), "MIR"] <- 1
+  
+  res$cross[which(res$cross[, "predicted"] == 0), "NIR"] <- 1
+  
+  res$MIR <- (sum(res$cross[, "MIR"]) / nrow(res$cross)) * 100
+  res$NIR <- (sum(res$cross[, "NIR"]) / nrow(res$cross)) * 100
+  
+  res$taxa_ids <- taxa_ids
+  
+  return(res)
 }
