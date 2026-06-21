4a93e1fc2110b697a779da2aac7d3814e50ecdac
Leonardo Dapporto <68218234+leondap@users.noreply.github.com>
2026-06-21 08:59:11 +0200

    Update biodecrypt_internal.R

--- R/biodecrypt_internal.R
+++ R/biodecrypt_internal.R
@@ -1,6 +1,7 @@
-  clean_geom <- function(x) {
+recluster_clean_geom <- function(x, crs_ref) {
+  
   x <- sf::st_make_valid(x)
-  sf::st_crs(x) <- sf::st_crs(matpunti)
+  sf::st_crs(x) <- crs_ref
   
   gt <- unique(as.character(sf::st_geometry_type(x)))
   
@@ -9,6 +10,7 @@
   }
   
   x <- x[!sf::st_is_empty(x), ]
+  
   x
 }
 
@@ -16,9 +18,9 @@
 
 
 
-
-area_safe <- function(x) {
-  x <- clean_geom(x)
+.recluster_area_safe <- function(x, crs_ref, crs_area = NULL) {
+  
+  x <- .recluster_clean_geom(x, crs_ref = crs_ref)
   
   if (!is.null(crs_area)) {
     x <- sf::st_transform(x, crs_area)
@@ -33,10 +35,10 @@ area_safe <- function(x) {
 
 
 
-
-intersection_area_safe <- function(x, y) {
-  x <- clean_geom(x)
-  y <- clean_geom(y)
+.recluster_intersection_area_safe <- function(x, y, crs_ref, crs_area = NULL) {
+  
+  x <- .recluster_clean_geom(x, crs_ref = crs_ref)
+  y <- .recluster_clean_geom(y, crs_ref = crs_ref)
   
   if (!is.null(crs_area)) {
     x <- sf::st_transform(x, crs_area)
@@ -46,21 +48,21 @@ intersection_area_safe <- function(x, y) {
   x <- sf::st_union(sf::st_make_valid(x))
   y <- sf::st_union(sf::st_make_valid(y))
   
-  z <- try(sf::st_intersection(x, y), silent = F)
+  z <- try(sf::st_intersection(x, y), silent = TRUE)
   
   if (inherits(z, "try-error") || is.null(z) || all(sf::st_is_empty(z))) {
     return(0)
   }
   
   z <- sf::st_make_valid(z)
-
-gt <- unique(as.character(sf::st_geometry_type(z)))
-
-if (any(gt == "GEOMETRYCOLLECTION")) {
-  z <- sf::st_collection_extract(z, "POLYGON")
-}
-
-z <- z[!sf::st_is_empty(z)]
+  
+  gt <- unique(as.character(sf::st_geometry_type(z)))
+  
+  if (any(gt == "GEOMETRYCOLLECTION")) {
+    z <- sf::st_collection_extract(z, "POLYGON")
+  }
+  
+  z <- z[!sf::st_is_empty(z)]
   
   if (length(z) == 0 || all(sf::st_is_empty(z))) {
     return(0)
